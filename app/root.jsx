58d9f0d59b079e60025b535e7d63bb5b4ec976fa
Jose Vazquez <jose.vazquez@pantheon.io>
2022-11-02 08:46:56 -0400

    updates 🙌

--- app/root.jsx
+++ app/root.jsx
@@ -5,17 +5,32 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
+
+import styles from './style.css';
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Remix on Pantheon',
+  viewport: 'width=device-width,initial-scale=1',
 });
+
+export const links = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Raleway+Dots&display=swap',
+    },
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+  ];
+};
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
